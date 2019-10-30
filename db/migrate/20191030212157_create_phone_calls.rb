5f58af64d91c84202e720eef70edcee45b3580b1
Andre Ferrer <andrerferrer@hotmail.com>
2019-10-30 18:22:39 -0300

    Create PhoneCall model

---
+++ db/migrate/20191030212157_create_phone_calls.rb
@@ -0,0 +1,12 @@
+class CreatePhoneCalls < ActiveRecord::Migration[6.0]
+  def change
+    create_table :phone_calls do |t|
+      t.string :called
+      t.string :caller
+      t.time :start_at
+      t.time :end_at
+
+      t.timestamps
+    end
+  end
+end
